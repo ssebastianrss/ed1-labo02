ba4a92b2263ed229c49a74b34f05f418b4e25f8d
Sebastian <sruanos@correo.url.edu.gt>
2025-03-24 17:58:11 -0600

    E01

--- src/main/java/ed/lab/E01InvertBT.java
+++ src/main/java/ed/lab/E01InvertBT.java
@@ -2,6 +2,12 @@ package ed.lab;
 
 public class E01InvertBT {
     public TreeNode<Integer> invertTree(TreeNode<Integer> root) {
+        if (root == null) return null;
+
+        TreeNode<Integer> temp = root.left;
+        root.left = invertTree(root.right);
+        root.right = invertTree(temp);
+
         return root;
     }
 }
