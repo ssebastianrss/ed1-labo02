6b33db1dbf357e2fe7df3db68dca242a967ed702
Sebastian <sruanos@correo.url.edu.gt>
2025-03-24 17:59:28 -0600

    E02

--- src/main/java/ed/lab/E02KthSmallest.java
+++ src/main/java/ed/lab/E02KthSmallest.java
@@ -2,8 +2,27 @@ package ed.lab;
 
 public class E02KthSmallest {
 
+    private int count = 0;
+    private int result = 0;
+
     public int kthSmallest(TreeNode<Integer> root, int k) {
-        return 0;
+        inorder(root, k);
+        return result;
     }
 
+    private void inorder(TreeNode<Integer> node, int k) {
+        if (node == null) {
+            return;
+        }
+
+        inorder(node.left, k);
+
+        count++;
+        if (count == k) {
+            result = node.value;
+            return;
+        }
+
+        inorder(node.right, k);
+    }
 }
